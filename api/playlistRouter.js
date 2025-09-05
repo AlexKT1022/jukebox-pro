655e54b8e4dc05c6f8b75e57656f966ea12241e1
AlexKT1022 <alexkt1022@gmail.com>
2025-09-05 18:02:02 -0400

    also refactored api routes

--- api/playlistRouter.js
+++ api/playlistRouter.js
@@ -1,6 +1,4 @@
 import express from 'express';
-const router = express.Router();
-export default router;
 
 import {
   createPlaylist,
@@ -10,47 +8,55 @@ import {
 import { createPlaylistTrack } from '#db/queries/playlists_tracks';
 import { getTracksByPlaylistId } from '#db/queries/tracks';
 
-router
-  .route('/')
-  .get(async (req, res) => {
-    const playlists = await getPlaylists();
-    res.send(playlists);
-  })
-  .post(async (req, res) => {
-    if (!req.body) return res.status(400).send('Request body is required.');
+const router = express.Router();
 
-    const { name, description } = req.body;
-    if (!name || !description)
-      return res.status(400).send('Request body requires: name, description');
+router.get('/', async (req, res) => {
+  const playlists = await getPlaylists();
 
-    const playlist = await createPlaylist(name, description);
-    res.status(201).send(playlist);
-  });
+  return res.send(playlists);
+});
+
+router.post('/', async (req, res) => {
+  if (!req.body) return res.status(400).send('Request body is required.');
+
+  const { name, description } = req.body;
+  if (!name || !description)
+    return res.status(400).send('Request body requires: name, description');
+
+  const newPlaylist = await createPlaylist(name, description);
+
+  return res.status(201).send(newPlaylist);
+});
 
 router.param('id', async (req, res, next, id) => {
   const playlist = await getPlaylistById(id);
+
   if (!playlist) return res.status(404).send('Playlist not found.');
 
   req.playlist = playlist;
+
   next();
 });
 
-router.route('/:id').get((req, res) => {
-  res.send(req.playlist);
+router.get('/:id', (req, res) => {
+  return res.send(req.playlist);
 });
 
-router
-  .route('/:id/tracks')
-  .get(async (req, res) => {
-    const tracks = await getTracksByPlaylistId(req.playlist.id);
-    res.send(tracks);
-  })
-  .post(async (req, res) => {
-    if (!req.body) return res.status(400).send('Request body is required.');
+router.get('/:id/tracks', async (req, res) => {
+  const tracks = await getTracksByPlaylistId(req.playlist.id);
 
-    const { trackId } = req.body;
-    if (!trackId) return res.status(400).send('Request body requires: trackId');
+  return res.send(tracks);
+});
 
-    const playlistTrack = await createPlaylistTrack(req.playlist.id, trackId);
-    res.status(201).send(playlistTrack);
-  });
+router.post('/:id/tracks', async (req, res) => {
+  if (!req.body) return res.status(400).send('Request body is required.');
+
+  const { trackId } = req.body;
+  if (!trackId) return res.status(400).send('Request body requires: trackId');
+
+  const playlistTrack = await createPlaylistTrack(req.playlist.id, trackId);
+
+  return res.status(201).send(playlistTrack);
+});
+
+export default router;
--- api/trackRouter.js
+++ api/trackRouter.js
@@ -4,13 +4,15 @@ export default router;
 
 import { getTrackById, getTracks } from '#db/queries/tracks';
 
-router.route('/').get(async (req, res) => {
+router.get('/', async (req, res) => {
   const tracks = await getTracks();
-  res.send(tracks);
+
+  return res.send(tracks);
 });
 
-router.route('/:id').get(async (req, res) => {
+router.get(':id', async (req, res) => {
   const track = await getTrackById(req.params.id);
   if (!track) return res.status(404).send('Track not found.');
-  res.send(track);
+
+  return res.send(track);
 });
